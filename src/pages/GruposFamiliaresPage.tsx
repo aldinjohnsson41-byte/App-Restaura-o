78c1102dc7580a42e5b845e260ecdc42f5b46b1a
aldinjohnsson41-byte <aldinjohnsson41@gmail.com>
2025-12-03 12:01:02 -0300

    Updated GruposFamiliaresPage.tsx

--- src/pages/GruposFamiliaresPage.tsx
+++ src/pages/GruposFamiliaresPage.tsx
@@ -89,15 +89,16 @@ export default function GruposFamiliaresPage({ onBack }: GruposFamiliaresPagePro
 
   async function loadGrupos() {
     const { data, error } = await supabase
-      .from('grupos_familiares')
-.select(`
-  *,
-  lider_1:id_lider_1_id ( id, nome_completo ),
-  lider_2:lider_2_id ( id, nome_completo ),
-  co_lider_1:co_lider_1_id ( id, nome_completo ),
-  co_lider_2:co_lider_2_id ( id, nome_completo )
-`)
-      .order('nome');
+  .from('grupos_familiares')
+  .select(`
+    *,
+    lider_1:lider_1_id ( id, nome_completo ),
+    lider_2:lider_2_id ( id, nome_completo ),
+    co_lider_1:co_lider_1_id ( id, nome_completo ),
+    co_lider_2:co_lider_2_id ( id, nome_completo )
+  `)
+  .order('nome');
+
 
     if (error) {
       console.error('Erro ao carregar grupos', error);
