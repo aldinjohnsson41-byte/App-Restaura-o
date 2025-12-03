2bdeabbfeb72c56830c74ef0f38fd9d775781112
aldinjohnsson41-byte <aldinjohnsson41@gmail.com>
2025-12-03 12:01:59 -0300

    Updated GruposFamiliaresPage.tsx

--- src/pages/GruposFamiliaresPage.tsx
+++ src/pages/GruposFamiliaresPage.tsx
@@ -90,7 +90,13 @@ export default function GruposFamiliaresPage({ onBack }: GruposFamiliaresPagePro
   async function loadGrupos() {
     const { data, error } = await supabase
       .from('grupos_familiares')
-      .select('*')
+.select(`
+  *,
+  lider_1:id_lider_1_id ( id, nome_completo ),
+  lider_2:lider_2_id ( id, nome_completo ),
+  co_lider_1:co_lider_1_id ( id, nome_completo ),
+  co_lider_2:co_lider_2_id ( id, nome_completo )
+`)
       .order('nome');
 
     if (error) {
